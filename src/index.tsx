c0208137d7f9bbaf0bd0d4d9284330d089a31085
John Popham <john@gocanopy.com>
2025-05-24 22:37:54 -0400

    Add debug log for Memberstack public key to diagnose Vercel env var issues

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MemberstackProvider } from '@memberstack/react';
+
+console.log('Memberstack public key:', process.env.REACT_APP_MEMBERSTACK_PUBLIC_KEY);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// NOTE: If you see a prop error, check the Memberstack React docs for the correct prop name (publicKey, projectId, or clientId)
 root.render(
   <React.StrictMode>
-    <App />
+    <MemberstackProvider config={{ publicKey: process.env.REACT_APP_MEMBERSTACK_PUBLIC_KEY! }}>
+      <App />
+    </MemberstackProvider>
   </React.StrictMode>
 ); 
